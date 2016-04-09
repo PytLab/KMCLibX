bf37a6d6ecac70d7421712f831a392cfd5cf3592
PytLab <shaozhengjiang@gmail.com>
2016-04-09 11:15:12 +0800

    Modify match list entry related classes presentation.
    1. Add new MinimalMatchListEntry class as the base class for other match list
    entry class
    2. Add its operation overloading functions.

--- c++/src/matchlistentry.h
+++ c++/src/matchlistentry.h
@@ -6,9 +6,19 @@
 */
 
 
-
-/*! \file  matchlistentry.h
- *  \brief File for the MatchListEntry class definition.
+/* ******************************************************************
+ *  file   : matchlistentry.h
+ *  brief  : File for the MatchListEntry class definition.
+ *  author : zjshao
+ *  date   : 2016-04-09
+ *
+ *  history:
+ *  <author>   <time>       <version>    <desc>
+ *  ------------------------------------------------------
+ *  zjshao     2016-04-09   1.2          Initial creation.
+ *
+ *  ------------------------------------------------------
+ * ******************************************************************
  */
 
 #ifndef __MATCHLISTENTRY__
@@ -17,124 +27,37 @@
 #include "coordinate.h"
 #include <cmath>
 
-/// A minimal struct to replace the MatchListEntry class.
-struct MinimalMatchListEntry {
+/// The base class for the match list entries.
+class MinimalMatchListEntry {
 
-    /// Flag for indicating if we have a move coordinate.
-    bool has_move_coordinate;
-
-    /// The match type integer.
-    int match_type;
-
-    /// The update type integer.
-    int update_type;
-
+public:
     /// The index in the global structure.
     int index;
-
-    /// The move vector cell component in the a direction.
-    int move_cell_i;
-
-    /// The move vector cell component in the b direction.
-    int move_cell_j;
-
-    /// The move vector cell component in the c direction.
-    int move_cell_k;
-
-    /// The basis component of the move vector.
-    int move_basis;
 
     /// The distance.
     double distance;
 
-    /// The coordinate.
+    /// The Coordinate.
     Coordinate coordinate;
 
-    /// The move coordinate.
-    Coordinate move_coordinate;
+    /// The match type integer.
+    int match_types;
 
+    /* Class functions. */
+    
+    /*! \brief 'less than' operator overloading for sorting matchlists.
+     */
+    virtual bool operator<(const MinimalMatchListEntry & other) const;
+
+    /*! \brief 'equal' operator overloading.
+     *  NOTE: This == operator dose not compare match types.
+     */
+    virtual bool operator==(const MinimalMatchListEntry & other) const;
+
+    /*! \brief 'not equal' operator overloading.
+     */
+    virtual bool operator==(const MinimalMatchListEntry & other) const;
 };
-
-
-/*! \brief The epsilon value for comparing lattice positions.
- */
-static double epsi__ = 1.0e-5;
-
-
-/*! \brief 'equal' for comparing points. NOTE: This operator does not
- *         compare match types.
- */
-inline
-bool operator==(const MinimalMatchListEntry & m1,
-                const MinimalMatchListEntry & m2)
-{
-    // Check the distance.
-    if (std::fabs(m2.distance - m1.distance) > epsi__)
-    {
-        return false;
-    }
-    // Check the coordinate.
-    else if (std::fabs(m2.coordinate.x() - m1.coordinate.x()) > epsi__)
-    {
-        return false;
-    }
-    else if (std::fabs(m2.coordinate.y() - m1.coordinate.y()) > epsi__)
-    {
-        return false;
-    }
-    else if (std::fabs(m2.coordinate.z() - m1.coordinate.z()) > epsi__)
-    {
-        return false;
-    }
-    return true;
-}
-
-
-
-/*! \brief 'not equal' for comparing entries.
- */
-inline
-bool operator!=(const MinimalMatchListEntry & m1,
-                const MinimalMatchListEntry & m2)
-{
-    // Handle the wildcard case.
-    if (m1.match_type == 0)
-    {
-        return false;
-    }
-    // Check the type.
-    else if (m2.match_type != m1.match_type)
-    {
-        return true;
-    }
-    else
-    {
-        return !(m1 == m2);
-    }
-}
-
-
-/*! \brief 'less than' for sorting matchlists.
- */
-inline
-bool operator<(const MinimalMatchListEntry & m1,
-               const MinimalMatchListEntry & m2)
-{
-
-    // Sort along distance, type and coordinate.
-    if (std::fabs(m2.distance - m1.distance) < epsi__)
-    {
-        // If the distances are practically the same,
-        // check the coordinate.
-        return (m1.coordinate < m2.coordinate);
-    }
-    else
-    {
-        // Sort wrt. distance.
-        return (m1.distance < m2.distance);
-    }
-}
-
 
 #endif // __MATCHLISTENTRY__
 
