544fe04d3352cad79f05e52a666d5e89c0bb9eb0
PytLab <shaozhengjiang@gmail.com>
2016-04-10 20:50:19 +0800

    Add match list type and configToMatchList() function.
    Do as Leetmaa in KMCLib 2.0a do.
    Seperate the match list from process and configuration for possible
    additional function adding. Seperation would make it easier to update the
    match list.

---
+++ c++/src/matchlist.h
@@ -0,0 +1,53 @@
+/*
+  This file is part of the KMCLib project distributed under the terms of the
+  GNU General Public License version 3, see <http://www.gnu.org/licenses/>.
+*/
+
+
+/* ******************************************************************
+ *  file   : matchlist.h
+ *  brief  : File for the MatchList definition and utility functions.
+ *
+ *  history:
+ *  <author>   <time>       <version>    <desc>
+ *  ------------------------------------------------------
+ *  zjshao     2016-04-10   1.2          Initial creation.
+ *
+ *  ------------------------------------------------------
+ * ******************************************************************
+ */
+
+#ifndef __MATCHLIST__
+#define __MATCHLIST__
+
+#include <vector>
+
+#include "matchlistentry.h"
+
+
+// Forward declarations, if any.
+class Configuration;
+
+
+/// Define the match lists.
+typedef std::vector<ProcessMatchListEntry> ProcessMatchList;
+typedef std::vector<ConfigMatchListEntry> ConfigMatchList;
+
+
+/*! \brief Set up a process matchlist from two local configurations.
+ *  \param first            : (in) The first (before) configuration.
+ *  \param second           : (in) The second (after) configuration.
+ *  \param range            : (in/out) The rang of the process.
+ *  \param cutoff           : (in/out) The cutoff of the process.
+ *  \param match_list       : (in/out) The matchlist to construct.
+ *  \param affected_indices : (in/out) The affected indices to set the size of.
+ */
+void configurationsToMatchList(const Configuration & first,
+                               const Configuration & second,
+                               int & range,
+                               double & cutoff,
+                               ProcessMatchList & match_list,
+                               std::vector<int> & affected_indices);
+
+#endif  // __MATCHLIST__
+
