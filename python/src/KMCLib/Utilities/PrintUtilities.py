1b5aee3a7b66671a85e5473fd1af0e24e6d5f308
PytLab <shaozhengjiang@gmail.com>
2016-04-24 18:39:44 +0800

    Change the copyright header.

--- python/src/KMCLib/Utilities/PrintUtilities.py
+++ python/src/KMCLib/Utilities/PrintUtilities.py
@@ -2,15 +2,21 @@
 
 
 # Copyright (c)  2013-2014  Mikael Leetmaa
+# Copyright (c)  2016-2019  Shao Zhengjiang
 #
-# This file is part of the KMCLib project distributed under the terms of the
+# This file is part of the KMCLibX project(based on KMCLib) distributed under the terms of the
 # GNU General Public License version 3, see <http://www.gnu.org/licenses/>.
 #
 
 
 import sys
 
-from KMCLib.Backend.Backend import MPICommons
+from ..Backend.Backend import MPICommons
+from .. import __version__
+from .. import __copyright__
+from .. import __email__
+from .. import __license__
+from .. import __author__
 
 
 def prettyPrint(msg, output=None):
@@ -32,6 +38,7 @@ def prettyPrint(msg, output=None):
         output.write("\n")
     MPICommons.barrier()
 
+
 def printHeader(output=None):
     """
     Utility function for printing the run header information.
@@ -44,18 +51,21 @@ def printHeader(output=None):
 
     # Write.
     prettyPrint("# -----------------------------------------------------------------------------", output)
-    prettyPrint("# KMCLib version 1.1.01", output)
-    prettyPrint("# Distributed under the GPLv3 license", output)
-    prettyPrint("# Copyright (C)  2012-2015  Mikael Leetmaa", output)
+    prettyPrint("# KMCLibX version " + __version__, output)
+    prettyPrint("# Distributed under the " + __license__ + " license", output)
+    prettyPrint("# " + __copyright__, output)
+    prettyPrint("# Developed by " + __author__ + " <" + __email__ + ">", output)
+    prettyPrint("#", output)
+    prettyPrint("# KMCLibX is developed based on KMCLib (https://github.com/leetmaa/KMCLib)", output)
     prettyPrint("# Developed by Mikael Leetmaa <leetmaa@kth.se>", output)
 
     prettyPrint("#", output)
-    prettyPrint("# This program is distributed in the hope that it will be useful", output);
-    prettyPrint("# but WITHOUT ANY WARRANTY; without even the implied warranty of", output);
-    prettyPrint("# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the", output);
-    prettyPrint("# LICENSE and README files, and the source code, for details.", output);
-    prettyPrint("#", output);
-    prettyPrint("# You should have received a copy of the GNU General Public License version 3", output);
-    prettyPrint("# (GPLv3) along with this program. If not, see <http://www.gnu.org/licenses/>.", output);
+    prettyPrint("# This program is distributed in the hope that it will be useful", output)
+    prettyPrint("# but WITHOUT ANY WARRANTY; without even the implied warranty of", output)
+    prettyPrint("# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the", output)
+    prettyPrint("# LICENSE and README files, and the source code, for details.", output)
+    prettyPrint("#", output)
+    prettyPrint("# You should have received a copy of the GNU General Public License version 3", output)
+    prettyPrint("# (GPLv3) along with this program. If not, see <http://www.gnu.org/licenses/>.", output)
     prettyPrint("# -----------------------------------------------------------------------------", output)
     prettyPrint("", output)
--- python/src/KMCLib/__init__.py
+++ python/src/KMCLib/__init__.py
@@ -2,10 +2,18 @@
 
 
 # Copyright (c)  2013  Mikael Leetmaa
+# Copyright (c)  2016-2019  Shao Zhengjiang
 #
-# This file is part of the KMCLib project distributed under the terms of the
+# This file is part of the KMCLibX project(based on KMCLib) distributed under the terms of the
 # GNU General Public License version 3, see <http://www.gnu.org/licenses/>.
 #
+
+__author__ = "Shao Zhengjiang"
+__copyright__ = "Copyright (C)  2016-2019  Shao Zhengjiang"
+__version__ = "2.0.0"
+__license__ = "GPLv3.0"
+__maintainer__ = __author__
+__email__ = "shaozhengjiang@gmail.com"
 
 
 from CoreComponents.KMCLocalConfiguration import KMCLocalConfiguration
@@ -23,6 +31,8 @@ from Utilities.SaveAndReadUtilities import KMCConfigurationFromScript
 from PluginInterfaces.KMCRateCalculatorPlugin import KMCRateCalculatorPlugin
 from PluginInterfaces.KMCAnalysisPlugin import KMCAnalysisPlugin
 from Backend.Backend import MPICommons
+from Utilities.PrintUtilities import printHeader
+
 
 __all__ = ['KMCLocalConfiguration', 'KMCInteractions', 'KMCConfiguration',
            'KMCLattice', 'KMCLatticeModel', 'KMCUnitCell',
@@ -35,7 +45,8 @@ __all__ = ['KMCLocalConfiguration', 'KMCInteractions', 'KMCConfiguration',
 MPICommons.init()
 
 # Print the header when the module is loaded.
-Utilities.PrintUtilities.printHeader()
+printHeader()
+
 
 # Make sure to finalize MPI on exit.
 def killme():
@@ -43,4 +54,3 @@ def killme():
 
 import atexit
 atexit.register(killme)
-
