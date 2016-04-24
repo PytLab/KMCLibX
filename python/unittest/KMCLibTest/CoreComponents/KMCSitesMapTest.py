b620f9d8a5f68f73bc90901b5fac55d0bbabf10b
PytLab <shaozhengjiang@gmail.com>
2016-04-24 22:37:43 +0800

    Add testLatticeMap() & testBackend().

--- python/unittest/KMCLibTest/CoreComponents/KMCSitesMapTest.py
+++ python/unittest/KMCLibTest/CoreComponents/KMCSitesMapTest.py
@@ -383,5 +383,88 @@ class KMCSitesMapTest(unittest.TestCase):
                           default_type=default_type)
         # }}}
 
+    def testLatticeMap(self):
+        """ Make sure the lattice map we get correspond to the lattice we give. """
+        # {{{
+        # Setup a valid KMCUnitCell.
+        unit_cell = KMCUnitCell(cell_vectors=numpy.array([[2.8, 0.0, 0.0],
+                                                          [0.0, 3.2, 0.0],
+                                                          [0.0, 0.5, 3.0]]),
+                                basis_points=[[0.0, 0.0, 0.0],
+                                              [0.5, 0.5, 0.5],
+                                              [0.25, 0.25, 0.75]])
+
+        # Setup the lattice.
+        lattice = KMCLattice(unit_cell=unit_cell,
+                             repetitions=(4, 4, 1),
+                             periodic=(True, True, False))
+
+        types = ['a', 'a', 'a', 'a', 'b', 'b',
+                 'a', 'a', 'a', 'b', 'b', 'b',
+                 'b', 'b', 'a', 'a', 'b', 'a',
+                 'b', 'b', 'b', 'a', 'b', 'a',
+                 'b', 'a', 'a', 'a', 'b', 'b',
+                 'b', 'b', 'b', 'b', 'b', 'b',
+                 'a', 'a', 'a', 'a', 'b', 'b',
+                 'b', 'b', 'a', 'b', 'b', 'a']
+
+        # Setup the configuration.
+        sitesmap = KMCSitesMap(lattice=lattice,
+                               types=types,
+                               possible_types=['a', 'c', 'b'])
+                                                            
+                                                            
+        # Get the lattice map.                              
+        cpp_lattice_map = sitesmap._latticeMap()              
+                                                            
+        # Get the map from the lattice.                     
+        cpp_lattice_map_ref = lattice._map()                
+
+        # Check that these two are references to the same underlying object.
+        self.assertTrue(cpp_lattice_map == cpp_lattice_map_ref)
+
+    def testBackend(self):
+        """ Make sure the C++ backend is what we expect. """
+        # {{{
+        # Setup a valid KMCUnitCell.
+        unit_cell = KMCUnitCell(cell_vectors=numpy.array([[2.8,0.0,0.0],
+                                                          [0.0,3.2,0.0],
+                                                          [0.0,0.5,3.0]]),
+                                basis_points=[[0.0,0.0,0.0],
+                                              [0.5,0.5,0.5],
+                                              [0.25,0.25,0.75]])
+
+        # Setup the lattice.
+        lattice = KMCLattice(unit_cell=unit_cell,
+                             repetitions=(4,4,1),
+                             periodic=(True,True,False))
+
+        types = ['a','a','a','a','b','b',
+                 'a','a','a','b','b','b',
+                 'b','b','a','a','b','a',
+                 'b','b','b','a','b','a',
+                 'b','a','a','a','b','b',
+                 'b','b','b','b','b','b',
+                 'a','a','a','a','b','b',
+                 'b','b','a','b','b','a']
+
+        # Setup the configuration.
+        sitesmap = KMCSitesMap(lattice=lattice,
+                               types=types,
+                               possible_types=['a','c','b'])
+
+        # Make sure that the backend stored on the config is None.
+        self.assertTrue(sitesmap._KMCConfiguration__backend is None)
+
+        # Query for the backend.
+        cpp_backend = sitesmap._backend()
+
+        # Check that the backend on the class is returned.
+        self.assertTrue(sitesmap._KMCConfiguration__backend == cpp_backend)
+
+        # Check the type of the cpp backend.
+        self.assertTrue(isinstance(cpp_backend, Backend.SitesMap))
+        # }}}
+
 if __name__ == '__main__':
     unittest.main()
